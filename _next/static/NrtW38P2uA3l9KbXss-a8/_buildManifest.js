daa1af8f6949802bc6e9be06bc1a3d6cd09a841d
blrok <blroktyabri@gmail.com>
2022-05-02 16:20:28 +0800

    Updates

---
+++ _next/static/NrtW38P2uA3l9KbXss-a8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-74abf5be4ffbbcd6.js","static/css/0bf2282ead3f5933.css","static/chunks/pages/index-54d0c43a7ddff6c1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
